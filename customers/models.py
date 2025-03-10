22a1d536156a72cfa8b4a6b87c7d5e0f8da14f50
Sushil Timilsina <developer.timilsina@gmail.com>
2025-03-10 14:17:57 +0545

    customer model complete

--- customers/models.py
+++ customers/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from books.models import Book
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -8,5 +9,22 @@ class Customer(models.Model):
    last_name = models.CharField(max_length=100)
    username = models.CharField(max_length=100, blank=True, unique=True)
    additional_info = models.TextField(blank=True)
-   rating = models.models.PositiveIntegerField(default=50)
+   rating = models.PositiveSmallIntegerField(default=50)
    books = models.ManyToManyField(Book, blank=True, help_text='books that are currently rented')
+   book_count = models.PositiveSmallIntegerField(default=0)
+
+   def __str__(self):
+      return str(self.username)
+   
+   def save(self, *args, **kwargs):
+      if not self.username:
+         username =slugify(f'{self.first_name} {self.last_name}')
+         ex = __class__.objects.filter(username = username).exists()
+
+         while ex:
+            i = len(__class__.objects.filter(first_name = self.first_name,last_name = self.last_name))
+            username = slugify(f'{self.first_name} {self.last_name} {i+1}')
+            ex = __class__.objects.filter(username = username).exists()
+         self.username = username
+
+      super().save(*args, **kwargs)
