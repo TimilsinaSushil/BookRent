7d6e98bada5821861d782886c54088af87de20b8
Sushil Timilsina <developer.timilsina@gmail.com>
2025-03-17 11:28:43 +0545

    method defination in model

--- core/views.py
+++ core/views.py
@@ -1,7 +1,17 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
+from customers.models import Customer
+from books.models import Book, BookTitle
 
 def home_view(request):
     # return HttpResponse("<h1>hello world</h1> <p>Hi there</P>")
-    value = "hello world 5"
-    return render(request, 'main.html', { 'value':value  })
+    qs = Customer.objects.all()
+    # obj = Book.objects.get(id=1)
+    obj = BookTitle.objects.get(id=1)
+    books = obj.get_books()
+    print(books)
+    context = {
+        'qs': qs,
+        'obj': obj
+    }
+    return render(request, 'main.html', context)
